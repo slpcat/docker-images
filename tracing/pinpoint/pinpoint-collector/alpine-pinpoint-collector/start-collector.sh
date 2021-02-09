57f13637639020bc1b35accbe2d0ab5e689c8872
slpcat <slpcat@qq.com>
2021-02-09 16:38:52 +0800

    pinpoint

--- tracing/pinpoint/pinpoint-collector/alpine-pinpoint-collector/start-collector.sh
+++ tracing/pinpoint/pinpoint-collector/alpine-pinpoint-collector/start-collector.sh
@@ -2,16 +2,16 @@
 set -e
 set -x
 
-sed -i "/cluster.enable=/ s/=.*/=${CLUSTER_ENABLE}/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i "/cluster.zookeeper.address=/ s/=.*/=${CLUSTER_ZOOKEEPER_ADDRESS}/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i "/flink.cluster.enable=/ s/=.*/=${FLINK_CLUSTER_ENABLE}/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i "/flink.cluster.zookeeper.address=/ s/=.*/=${FLINK_CLUSTER_ZOOKEEPER_ADDRESS}/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
+sed -i "/cluster.enable=/ s/=.*/=${CLUSTER_ENABLE}/" /pinpoint/config/pinpoint-collector.properties
+sed -i "/cluster.zookeeper.address=/ s/=.*/=${CLUSTER_ZOOKEEPER_ADDRESS}/g" /pinpoint/config/pinpoint-collector.properties
+sed -i "/flink.cluster.enable=/ s/=.*/=${FLINK_CLUSTER_ENABLE}/" /pinpoint/config/pinpoint-collector.properties
+sed -i "/flink.cluster.zookeeper.address=/ s/=.*/=${FLINK_CLUSTER_ZOOKEEPER_ADDRESS}/" /pinpoint/config/pinpoint-collector.properties
 
-sed -i "/hbase.client.host=/ s/=.*/=${HBASE_HOST}/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/hbase.properties
-sed -i "/hbase.client.port=/ s/=.*/=${HBASE_PORT}/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/hbase.properties
-sed -i "/base.zookeeper.znode.parent=/ s/=.*/=\/${HBASE_ZNODE_PARENT}/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/hbase.properties
-#sed -i "/hbase.namespace=/ s/=.*/=${HBASE_NAMESPACE}/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/hbase.properties
+sed -i "/hbase.client.host=/ s/=.*/=${HBASE_HOST}/" /pinpoint/config/hbase.properties
+sed -i "/hbase.client.port=/ s/=.*/=${HBASE_PORT}/" /pinpoint/config/hbase.properties
+sed -i "/base.zookeeper.znode.parent=/ s/=.*/=\/${HBASE_ZNODE_PARENT}/" /pinpoint/config/hbase.properties
+#sed -i "/hbase.namespace=/ s/=.*/=${HBASE_NAMESPACE}/" /pinpoint/config/hbase.properties
 
-sed -i "/level value=/ s/=.*/=\"${DEBUG_LEVEL}\"\/>/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/log4j.xml
+sed -i "/level value=/ s/=.*/=\"${DEBUG_LEVEL}\"\/>/g" /pinpoint/config/log4j.xml
 
-exec /usr/local/tomcat/bin/catalina.sh run
+exec java -jar /pinpoint/pinpoint-collector-boot.jar --spring.config.additional-location=/pinpoint/config/pinpoint-collector.properties
