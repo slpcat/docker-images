3209c618e75a2a2dd42a588a1f1f36305fe307ad
slpcat <slpcat@qq.com>
2021-02-18 01:09:24 +0800

    pp-web

--- tracing/pinpoint/pinpoint-web/alpine-pinpoint-web/start-web.sh
+++ tracing/pinpoint/pinpoint-web/alpine-pinpoint-web/start-web.sh
@@ -2,4 +2,4 @@
 set -e
 set -x
 
-exec java -jar /pinpoint/pinpoint-web-boot.jar --spring.config.additional-location=/pinpoint/config/pinpoint-web.properties/pinpoint
+exec java -jar /pinpoint/pinpoint-web-boot.jar --spring.config.additional-location=/pinpoint/config/pinpoint-web.properties
