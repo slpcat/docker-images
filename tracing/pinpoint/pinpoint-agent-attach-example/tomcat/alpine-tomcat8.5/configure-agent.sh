68b1e9e8f3709276ef45e6205987a783b6922b05
slpcat <slpcat@qq.com>
2021-02-18 00:26:51 +0800

    pinpoint

--- tracing/pinpoint/pinpoint-agent-attach-example/tomcat/alpine-tomcat8.5/configure-agent.sh
+++ tracing/pinpoint/pinpoint-agent-attach-example/tomcat/alpine-tomcat8.5/configure-agent.sh
@@ -2,11 +2,11 @@
 set -e
 set -x
 
-sed -i "/profiler.collector.ip=/ s/=.*/=${COLLECTOR_IP}/" /pinpoint-agent/pinpoint-root.config
-sed -i "/profiler.collector.tcp.port=/ s/=.*/=${COLLECTOR_TCP_PORT}/" /pinpoint-agent/pinpoint-root.config
-sed -i "/profiler.collector.stat.port=/ s/=.*/=${COLLECTOR_STAT_PORT}/" /pinpoint-agent/pinpoint-root.config
-sed -i "/profiler.collector.span.port=/ s/=.*/=${COLLECTOR_SPAN_PORT}/" /pinpoint-agent/pinpoint-root.config
-sed -i "/profiler.sampling.rate=/ s/=.*/=${PROFILER_SAMPLING_RATE}/" /pinpoint-agent/pinpoint-root.config
+sed -i "/profiler.collector.ip=/ s/=.*/=${COLLECTOR_IP}/" /pinpoint-agent/pinpoint-agent-2.2.1/pinpoint-root.config
+sed -i "/profiler.collector.tcp.port=/ s/=.*/=${COLLECTOR_TCP_PORT}/" /pinpoint-agent/pinpoint-agent-2.2.1/pinpoint-root.config
+sed -i "/profiler.collector.stat.port=/ s/=.*/=${COLLECTOR_STAT_PORT}/" /pinpoint-agent/pinpoint-agent-2.2.1/pinpoint-root.config
+sed -i "/profiler.collector.span.port=/ s/=.*/=${COLLECTOR_SPAN_PORT}/" /pinpoint-agent/pinpoint-agent-2.2.1/pinpoint-root.config
+sed -i "/profiler.sampling.rate=/ s/=.*/=${PROFILER_SAMPLING_RATE}/" /pinpoint-agent/pinpoint-agent-2.2.1/pinpoint-root.config
 
 #sed -i "/level value=/ s/=.*/=\"${DEBUG_LEVEL}\"\/>/g" /pinpoint-agent/lib/log4j.xml
 
