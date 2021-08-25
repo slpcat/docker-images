ea61638b6e598bf9e657ef3fcd6e6eccae21437b
slpcat <slpcat@qq.com>
2021-08-25 16:58:06 +0800

    logstash java

---
+++ logging/logstash/centos7-logstash/java.sh
@@ -0,0 +1,7 @@
+#set java environment
+JAVA_HOME=/usr/share/logstash/jdk
+#JRE_HOME=$JAVA_HOME/jre
+#PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin
+PATH=$PATH:$JAVA_HOME/bin
+CLASSPATH=.:$JAVA_HOME/lib
+export JAVA_HOME JRE_HOME PATH CLASSPATH
