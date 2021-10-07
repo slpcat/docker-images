3e9e015b55691c72e6bacb6ea8e97f97a1bd5718
slpcat <slpcat@qq.com>
2021-10-07 23:28:34 +0800

    s6

---
+++ docker-desktop/xrdp/docker-baseimage-ubuntu/install_s6.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+    if [ "$TARGETARCH" == "amd64" ]; then
+       export ARCH="amd64"
+    fi
+
+    if [ "$TARGETARCH" == "arm64" ]; then
+       export ARCH="aarch64"
+    fi
+
+    wget -O /tmp/s6-overlay.tar.gz https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/s6-overlay-${ARCH}.tar.gz
+    tar xvfz /tmp/s6-overlay.tar.gz -C / && rm /tmp/s6-overlay.tar.gz && ln -sf /usr/bin/bash /bin/sh && ln -sf /usr/bin/bash /bin/bash
+    rm -rf /tmp/*
