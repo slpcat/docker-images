1ebe41a6c2f8b77fa9414444b6b66f1b4ce5815d
slpcat <slpcat@qq.com>
2023-05-21 10:16:51 +0800

    sd

---
+++ aigc/stable-diffusion-webui/comfy/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+mkdir -vp /data/config/comfy/custom_nodes
+
+declare -A MOUNTS
+
+MOUNTS["/root/.cache"]="/data/.cache"
+MOUNTS["${ROOT}/input"]="/data/config/comfy/input"
+MOUNTS["${ROOT}/output"]="/output/comfy"
+
+for to_path in "${!MOUNTS[@]}"; do
+  set -Eeuo pipefail
+  from_path="${MOUNTS[${to_path}]}"
+  rm -rf "${to_path}"
+  if [ ! -f "$from_path" ]; then
+    mkdir -vp "$from_path"
+  fi
+  mkdir -vp "$(dirname "${to_path}")"
+  ln -sT "${from_path}" "${to_path}"
+  echo Mounted $(basename "${from_path}")
+done
+
+exec "$@"
---
+++ aigc/stable-diffusion-webui/invoke/entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+declare -A MOUNTS
+
+mkdir -p ${CONFIG_DIR} ${ROOT}/configs/stable-diffusion/
+
+# cache
+MOUNTS["/root/.cache"]=/data/.cache/
+
+# this is really just a hack to avoid migrations
+rm -rf ${HF_HOME}/diffusers
+
+# ui specific
+MOUNTS["${ROOT}/models/codeformer"]=/data/Codeformer/
+MOUNTS["${ROOT}/models/gfpgan/GFPGANv1.4.pth"]=/data/GFPGAN/GFPGANv1.4.pth
+MOUNTS["${ROOT}/models/gfpgan/weights"]=/data/GFPGAN/
+MOUNTS["${ROOT}/models/realesrgan"]=/data/RealESRGAN/
+
+MOUNTS["${ROOT}/models/ldm"]=/data/.cache/invoke/ldm/
+
+# hacks
+
+for to_path in "${!MOUNTS[@]}"; do
+  set -Eeuo pipefail
+  from_path="${MOUNTS[${to_path}]}"
+  rm -rf "${to_path}"
+  mkdir -p "$(dirname "${to_path}")"
+  # ends with slash, make it!
+  if [[ "$from_path" == */ ]]; then
+    mkdir -vp "$from_path"
+  fi
+
+  ln -sT "${from_path}" "${to_path}"
+  echo Mounted $(basename "${from_path}")
+done
+
+if "${PRELOAD}" == "true"; then
+  set -Eeuo pipefail
+  invokeai-configure --root ${ROOT} --yes
+  cp ${ROOT}/configs/models.yaml ${CONFIG_DIR}/models.yaml
+fi
+
+exec "$@"
