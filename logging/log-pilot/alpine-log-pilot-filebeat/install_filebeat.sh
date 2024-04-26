97aaa6963a2d233a67dccb8f5debc6181e0687af
slpcat <slpcat@qq.com>
2024-04-26 11:44:54 +0800

    log-pilot

--- logging/log-pilot/alpine-log-pilot-filebeat/install_filebeat.sh
+++ logging/log-pilot/alpine-log-pilot-filebeat/install_filebeat.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/bash
     if [ "$TARGETARCH" == "amd64" ]; then
        export ARCH="x86_64"
     else
        export ARCH=$TARGETARCH
     fi
 
-    wget  https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-${FILEBEAT_VERSION}-linux-${ARCH}.tar.gz
-    tar xzvf filebeat-${FILEBEAT_VERSION}-linux-${ARCH}.tar.gz
-    mv filebeat-${FILEBEAT_VERSION}-linux-${ARCH}/filebeat /usr/bin/filebeat
-    rm -rf filebeat-${FILEBEAT_VERSION}-linux-${ARCH}*
+    cd /tmp
+    wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-${FILEBEAT_VERSION}-linux-${ARCH}.tar.gz
+    mkdir -p /etc/filebeat /var/lib/filebeat /var/log/filebeat
+    tar zxf /tmp/filebeat-${FILEBEAT_VERSION}-linux-${ARCH}.tar.gz -C /tmp/
+    cp -rf /tmp/filebeat-${FILEBEAT_VERSION}-linux-${ARCH}/filebeat /usr/bin/
+    cp -rf /tmp/filebeat-${FILEBEAT_VERSION}-linux-${ARCH}/fields.yml /etc/filebeat/
+    cp -rf /tmp/filebeat-${FILEBEAT_VERSION}-linux-${ARCH}/kibana /etc/filebeat/
+    cp -rf /tmp/filebeat-${FILEBEAT_VERSION}-linux-${ARCH}/module /etc/filebeat/
+    cp -rf /tmp/filebeat-${FILEBEAT_VERSION}-linux-${ARCH}/modules.d /etc/filebeat/
