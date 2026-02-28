1087af9a6794dc1fff43dd471987aa5ff70ff68c
slpcat <slpcat@qq.com>
2026-02-28 16:00:04 +0800

    update shell

--- docker-desktop/linuxserver/docker-webtop-ubuntu2404-xfce/root/etc/skel/creator-install.sh
+++ docker-desktop/linuxserver/docker-webtop-ubuntu2404-xfce/root/etc/skel/creator-install.sh
@@ -103,37 +103,3 @@ export PATH="$HOME/.local/bin:$PATH"
 #https://github.com/TNTwise/REAL-Video-Enhancer
 #flatpak install -y flathub io.github.tntwise.REAL-Video-Enhancer
 
-############################################################
-
-#KiCad's Schematic Editor supports everything from the most basic schematic to a complex hierarchical design with hundreds of sheets.
-
-#LibrePCB
-#Horizon EDA
-#Fritzing
-#gEDA
-
-#ngspice
-#Qucs-S
-#Xyce
-#
-
-#scilab
-#GNU Octave
-
-#OpenFOAM
-#CalculiX
-#Elmer FEM
-#Bioconductor
-#BLAST/EMBOSS
-
-#LibreCAD
-#FreeCAD
-#OpenSCAD
-#SolveSpace
-#BRL-CAD
-
-#LCEDA (Chinese: 嘉立创EDA), Chinese editon of EasyEDA, is a web-based, ready-to-use, easy-to-use, team-collaborative EDA tool.
-#flatpak install flathub cn.lceda.LCEDAPro
-#EasyEDA, the international edition of LCEDA, is a web-based, ready-to-use, easy-to-use, team-collaborative EDA tool.
-#flatpak install flathub com.easyeda.EasyEDAPro
-
