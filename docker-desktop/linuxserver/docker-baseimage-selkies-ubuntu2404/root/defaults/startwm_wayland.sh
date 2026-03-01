381f1768afe8c01f74707d4359b339f03f427fc9
slpcat <slpcat@qq.com>
2026-03-01 13:52:15 +0800

    add base

---
+++ docker-desktop/linuxserver/docker-baseimage-selkies-ubuntu2404/root/defaults/startwm_wayland.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Start DE
+ulimit -c 0
+export XCURSOR_THEME=breeze
+export XCURSOR_SIZE=24
+export XKB_DEFAULT_LAYOUT=us
+export XKB_DEFAULT_RULES=evdev
+export WAYLAND_DISPLAY=wayland-1
+labwc > /dev/null 2>&1
